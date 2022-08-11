d3c33c12b3fa8b335137be590e4a4d50d031b054
thisisbipin <bipinmjadav087@gmail.com>
2022-08-11 20:57:21 +0530

    updated the new UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
 import "./App.css";
-import Header from "./components/Header";
-import Mic from "./components/Mic";
 
-function App() {
+export const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true,
+});
+
+const App = () => {
+    const [landingPageData, setLandingPageData] = useState({});
+    useEffect(() => {
+        setLandingPageData(JsonData);
+    }, []);
+
     return (
-        <div className="App">
-            <Header />
-            <Mic />
+        <div>
+            <Navigation />
+            <Header data={landingPageData.Header} />
+            <Features data={landingPageData.Features} />
+            <About data={landingPageData.About} />
+            <Team data={landingPageData.Team} />
+            <Contact data={landingPageData.Contact} />
         </div>
     );
-}
+};
 
 export default App;
